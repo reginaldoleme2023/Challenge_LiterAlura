10b120773bba10fca86429b785ab37c17f7a38d3
reginaldoleme2023 <reginaldoleme2018@gmail.com>
2024-05-27 12:53:19 -0300

    basic project

---
+++ src/main/java/br/com/alura/ChallengeLiteralura/ChallengeLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.ChallengeLiteralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraluraApplication.class, args);
+	}
+
+}
